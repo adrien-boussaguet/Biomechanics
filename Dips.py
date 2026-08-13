aba789780375d7f6a94dfae87af202bc18bdcb48
Adrien Boussaguet <adrien.boussaguet@gmail.com>
2026-08-13 18:30:10 +0200

    update Dips.py, 'Shoulder Tracking.csv' and Trajectory.py

--- Dips.py
+++ Dips.py
@@ -1,5 +1,7 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 #Masses
 m_body = 80 #kg
@@ -23,9 +25,35 @@ la_elbow = 0.025    #m
 I_shoulder = 42.5
 la_shoulder = 0.045
 
-theta1 = np.radians(-5)  # angle of the forearm with respect to the vertical
-theta2 = np.radians(100) # angle of the upper arm with respect to the vertical
-theta3 = np.radians(180) # angle of the torso with respect to the vertical
+#Extraction CSV
+#,'G:\Documents\VS Code\Biomecha\Biomechanics\Tracking\Shoulder Tracking.csv'
+files_path = ['G:\Documents\VS Code\Biomecha\Biomechanics\Tracking\Elbow Tracking.csv','G:\Documents\VS Code\Biomecha\Biomechanics\Tracking\Shoulder Tracking.csv']
+
+list_angle_rad = []
+
+for i in range(len(files_path)):
+    df = pd.read_csv(files_path[i], sep=';', decimal=',', skiprows=0)
+
+    time = df.iloc[:, 0]
+
+    ox, oy = df.iloc[:, 7], df.iloc[:, 8]
+    ax, ay = df.iloc[:, 9], df.iloc[:, 10]
+    bx, by = df.iloc[:, 11], df.iloc[:, 12]
+
+    oa_x, oa_y = ax - ox, ay - oy
+    ob_x, ob_y = bx - ox, by - oy
+
+    dot_product = oa_x * ob_x + oa_y * ob_y
+    norm_oa = np.sqrt(oa_x**2 + oa_y**2)
+    norm_ob = np.sqrt(ob_x**2 + ob_y**2)
+
+    cos_theta = np.clip(dot_product / (norm_oa * norm_ob), -1.0, 1.0)
+    angle_rad = np.arccos(cos_theta)
+
+    list_angle_rad.append(angle_rad)
+
+All_angle_rad = pd.concat(list_angle_rad, axis=1)
+print(All_angle_rad)
 
 #Position des articulations
 P_hand=np.array([0,0])
@@ -33,56 +61,67 @@ P_elbow=L1*np.array([np.cos(theta1), np.sin(theta1)])
 P_shoulder=P_elbow + L2*np.array([np.cos(theta2), np.sin(theta2)])
 G_bust=P_shoulder+d3*np.array([np.cos(theta3), np.sin(theta3)])
 
-#Fonction calcul de moment
+#def torque_elbow(theta1, theta2, theta3, alpha2):
+    #P_elbow=L1*np.array([np.cos(theta1), np.sin(theta1)])
+    #P_shoulder=P_elbow + L2*np.array([np.cos(theta2), np.sin(theta2)])
+    #G_bust=P_shoulder+d3*np.array([np.cos(theta3), np.sin(theta3)])
 
-def torque_elbow(theta1, theta2, theta3, alpha2):
-    P_elbow=L1*np.array([np.cos(theta1), np.sin(theta1)])
-    P_shoulder=P_elbow + L2*np.array([np.cos(theta2), np.sin(theta2)])
-    G_bust=P_shoulder+d3*np.array([np.cos(theta3), np.sin(theta3)])
+    #gravity_lever_arm = G_bust[1] - P_elbow[1]
+    #tau_ext_elbow = - (m_bust * g * gravity_lever_arm)
 
-    gravity_lever_arm = G_bust[1] - P_elbow[1]
-    tau_ext_elbow = - (m_bust * g * gravity_lever_arm)
+    #tau_int_elbow = I_elbow * alpha2 - tau_ext_elbow
+    #triceps_force = tau_int_elbow / la_elbow
 
-    tau_int_elbow = I_elbow * alpha2 - tau_ext_elbow
-    triceps_force = tau_int_elbow / la_elbow
+    #return tau_ext_elbow, tau_int_elbow, triceps_force
 
-    return tau_ext_elbow, tau_int_elbow, triceps_force
+#def torque_shoulder(theta1, theta2, theta3, alpha3):
+    #P_elbow=L1*np.array([np.cos(theta1), np.sin(theta1)])
+    #P_shoulder=P_elbow + L2*np.array([np.cos(theta2), np.sin(theta2)])
+    #G_bust=P_shoulder+d3*np.array([np.cos(theta3), np.sin(theta3)])
 
-alpha2=1
-tau_ext_elbow, tau_int_elbow, triceps_force = torque_elbow(theta1, theta2, theta3, alpha2)
-#print(tau_ext_elbow, tau_int_elbow, triceps_force)
+    #gravity_lever_arm = G_bust[1] - P_shoulder[1]
+    #tau_ext_shoulder = - (m_bust * g * gravity_lever_arm)
 
-def torque_shoulder(theta1, theta2, theta3, alpha3):
-    P_elbow=L1*np.array([np.cos(theta1), np.sin(theta1)])
-    P_shoulder=P_elbow + L2*np.array([np.cos(theta2), np.sin(theta2)])
-    G_bust=P_shoulder+d3*np.array([np.cos(theta3), np.sin(theta3)])
+    #tau_int_shoulder = I_shoulder * alpha3 - tau_ext_shoulder
+    #pec_force = tau_int_shoulder / la_shoulder
 
-    gravity_lever_arm = G_bust[1] - P_shoulder[1]
-    tau_ext_shoulder = - (m_bust * g * gravity_lever_arm)
-
-    tau_int_shoulder = I_shoulder * alpha3 - tau_ext_shoulder
-    pec_force = tau_int_shoulder / la_shoulder
-
-    return tau_ext_shoulder, tau_int_shoulder, pec_force
-
-alpha3=1
-tau_ext_shoulder, tau_int_shoulder, pec_force = torque_shoulder(theta1, theta2, theta3, alpha3)
-#print(tau_ext_shoulder, tau_int_shoulder, pec_force)
-
-time = np.linspace(0, 3.0, 100)
-theta2_high = np.radians(10) #Position haute de l'angle du coude
-theta2_low = np.radians(100) #position basse de l'angle du coude
-theta2_traj = theta2_low + (theta2_high - theta2_low) * (1 + np.cos(2 * np.pi * time / 3.0)) / 2
-
-
+    #return tau_ext_shoulder, tau_int_shoulder, pec_force
 
 #Graphs
 x=[P_hand[1], P_elbow[1], P_shoulder[1], G_bust[1]]
 y=[P_hand[0], P_elbow[0], P_shoulder[0], G_bust[0]]
 
-plt.figure(figsize=(6, 6))
-plt.plot(x, y, 'o-')
-plt.axhline(y=0, color='k', linestyle='--')
-plt.xlim(-0.75, 0.75)
-plt.ylim(-0.75, 0.75)
+def animate(i):
+    P_hand = np.array([0, 0])
+    P_elbow = L1 * np.array([np.cos(theta1[i]), np.sin(theta1[i])])
+    P_shoulder = P_elbow + L2 * np.array([np.cos(theta2[i]), np.sin(theta2[i])])
+    G_bust = P_shoulder + d3 * np.array([np.cos(theta3[i]), np.sin(theta3[i])])
+    
+    # Coordonnées de l'instant t (avec ton inversion des axes [1] pour x et [0] pour y)
+    x = [P_hand[1], P_elbow[1], P_shoulder[1], G_bust[1]]
+    y = [P_hand[0], P_elbow[0], P_shoulder[0], G_bust[0]]
+    
+    # Mise à jour du corps
+    ligne_corps.set_data(x, y)
+    
+    # Mise à jour de l'historique et de la trajectoire du COUDE
+    hist_x_coude.append(P_elbow[1])
+    hist_y_coude.append(P_elbow[0])
+    trajectoire_coude.set_data(hist_x_coude, hist_y_coude)
+    
+    # Mise à jour de l'historique et de la trajectoire de l'ÉPAULE
+    hist_x_epaule.append(P_shoulder[1])
+    hist_y_epaule.append(P_shoulder[0])
+    trajectoire_epaule.set_data(hist_x_epaule, hist_y_epaule)
+    
+    # Mise à jour de l'historique et de la trajectoire du BUSTE
+    hist_x_buste.append(G_bust[1])
+    hist_y_buste.append(G_bust[0])
+    trajectoire_buste.set_data(hist_x_buste, hist_y_buste)
+    
+    return ligne_corps, trajectoire_coude, trajectoire_epaule, trajectoire_buste
+
+# Création de l'animation
+ani = animation.FuncAnimation(fig, animate, frames=frames, interval=30, blit=True)
+
 plt.show()
